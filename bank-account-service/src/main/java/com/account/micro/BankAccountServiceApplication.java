fa79d65b64487f528d270f410195ac1af1198c56
Bhushan <nagabhushan@altimetrik.com>
2019-07-11 17:38:34 +0530

    Eureka fixes

--- bank-account-service/src/main/java/com/account/micro/BankAccountServiceApplication.java
+++ bank-account-service/src/main/java/com/account/micro/BankAccountServiceApplication.java
@@ -2,11 +2,12 @@ package com.account.micro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.account.micro")
-public class BankAccountServiceApplication {
+@EnableDiscoveryClient
+@ComponentScan(basePackages = "com.account.micro") public class BankAccountServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankAccountServiceApplication.class, args);
--- bank-customer-service/src/main/java/com/micro/springbootdata/SpringbootdataApplication.java
+++ bank-customer-service/src/main/java/com/micro/springbootdata/SpringbootdataApplication.java
@@ -2,15 +2,13 @@ package com.micro.springbootdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
-//@EnableDiscoveryClient
+@SpringBootApplication @EnableDiscoveryClient
 //@ComponentScan(basePackages = "com.micro")
 public class SpringbootdataApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootdataApplication.class, args);
 	}
-
 }
